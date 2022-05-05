e11e8265a5adeba414b39cbd9636fefa0e1ba9d3
JOSHHH <josh0039@e.ntu.edu.sg>
2022-05-05 22:58:11 +0800

    Created tab 2

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <div id="header">
           <p>Navbar tab1</p>
+          <p>Navbar tab2</p>
         </div>
         <p>
           Edit <code>src/App.js</code> and save to reloadsssss.
