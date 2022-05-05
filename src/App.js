d6fcd5b5ceb7e8e3d89db8aa4fbce06d8f66b2b0
JOSHHH <josh0039@e.ntu.edu.sg>
2022-05-05 23:14:56 +0800

    charlie commmit

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React2
         </a>
         <div>
           <p>Body1</p>
